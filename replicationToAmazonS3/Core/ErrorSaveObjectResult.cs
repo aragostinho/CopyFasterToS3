f2faf05fd16208cbdc2e1e1201f6aaf41c1188fb
aragostinho <andre@magnadev.com.br>
2019-04-28 21:26:07 -0300

    New feature: Catch log error

---
+++ replicationToAmazonS3/Core/ErrorSaveObjectResult.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace replicationToAmazonS3.Core
+{
+    public class ErrorSaveObjectResult
+    {
+        public string Message { get; protected set; }
+        public string FilePath { get; protected set; }
+        public Stream FileStream { get; protected set; }
+        public string KeyName { get; protected set; }
+        public Exception Exception { get; protected set; }
+
+        public ErrorSaveObjectResult(string message, Exception exception, string filePath, string keyName)
+        {
+            this.Message = message;
+            this.Exception = exception;
+            this.FilePath = filePath;
+            this.KeyName = keyName;
+        }
+
+        public ErrorSaveObjectResult(string message, Exception exception, Stream fileStream, string keyName)
+        {
+            this.Message = message;
+            this.Exception = exception;
+            this.FileStream = fileStream;
+            this.KeyName = keyName;
+        }
+    }
+}
